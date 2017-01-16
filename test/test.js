78a4ab9d0cc12605befb376b2aeba078bb5010b6
tzofiaCo <ctzofia@gmail.com>
2017-01-16 11:11:53 +0200

    RED:Cash Register getChange(486,1000) should equal [500, 10, 2, 2]

--- test/test.js
+++ test/test.js
@@ -12,4 +12,7 @@ describe('Cash Register', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
     })
   })
+  it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+})
 });
