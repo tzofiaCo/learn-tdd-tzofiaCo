fdc73dc61cf126ddd35945582ee8b8f1e475cea5
tzofiaCo <ctzofia@gmail.com>
2017-01-16 13:12:37 +0200

    RED: 1) getChange(486,1000) should equal [500, 10, 2, 2]

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require("assert"); // core module
 var C = require('../cash.js');  // our module
-totalPayable = 210         // £2.10
-cashPaid     = 300         // £3.00
-dfference    =  90         // 90p
-change       = [50,20,20]  // 50p, 20p, 20p
+totalPayable = 486           // £4.86
+cashPaid     = 1000          // £10.00
+dfference    = 514           // £5.14
+change       = [500,10,2,2]  // £5, 10p, 2p, 2p
 describe('Cash Register', function(){
   describe('Module C', function(){
     it('should have a getChange Method', function(){
@@ -12,6 +12,9 @@ describe('Cash Register', function(){
     })
 	it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
+it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
 })
   })
 });
